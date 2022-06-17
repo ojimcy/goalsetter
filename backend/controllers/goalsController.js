52048e3e50cd58be0f780f6e783d6d82afdcbf9a
ojimcy <ojimcy247@gmail.com>
2022-06-17 12:45:24 +0100

    Error handler middleware

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -9,6 +9,12 @@ const getGoals = (req, res) => {
 // @route  post /api/goals
 // @access Private
 const setGoal = (req, res) => {
+    console.log(req.params);
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({message: "Set goal"})
 }
 
