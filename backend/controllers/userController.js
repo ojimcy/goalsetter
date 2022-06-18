cad09151ff98a956c0e176f0cef37b21321495b6
ojimcy <ojimcy247@gmail.com>
2022-06-18 14:57:26 +0100

    added JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -60,7 +61,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json({
         _id: user.id,
         name: user.name,
-        email: user.email   
+        email: user.email,
+        token: generateToken(user._id)   
     })
    } else {
     res.status(400)
@@ -77,6 +79,13 @@ const getUser = asyncHandler(async (req, res) => {
         message: 'User data displayed'
     })
 })
+
+// Generate JWT 
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 
 module.exports = {
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,19 +3,18 @@ const mongoose = require("mongoose");
 const goalSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Goal",
+  },
     text: {
       type: String,
-      required: [true, "Please add a text value"],
+      required: [true, "Please add text value"],
     },
   },
-
-  {
-    timestamps: true,
-  }
+  { 
+    timestamps: true
+   }
 );
 
-module.exports = mongoose.model("Goal", goalSchema);
+module.exports = mongoose.model('Goal', goalSchema)
